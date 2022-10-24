452226822fb01763d06f59f9631dff31c4e5b29f
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 16:18:08 -0300

    Create main file structure

--- gamepy/game.py
+++ gamepy/game.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from models.calculate import Calcular
+
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+
+def jogar(pontos: int) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
