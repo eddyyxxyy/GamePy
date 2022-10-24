13e20e6135e7d582237c3a607ff4329b1a80a043
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 16:40:38 -0300

    Finishes the project

--- gamepy/game.py
+++ gamepy/game.py
@@ -10,7 +10,28 @@ def main() -> None:
 
 
 def jogar(pontos: int) -> None:
-    pass
+    dificuldade: int = int(
+        input('Informe o nível de dificuldade desejado: [1, 2, 3 ou 4]\n-> ')
+    )
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a seguinte operação: ')
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} ponto(s).')
+
+    if continuar := int(
+        input('Deseja continuar no jogo? [1 - Sim, 0 - Não]\n-> ')
+    ):
+        jogar(pontos)
+    else:
+        print(f'Você finalizou com {pontos} ponto(s).')
+        print('Até a próxima!')
 
 
 if __name__ == '__main__':
--- gamepy/models/calculate.py
+++ gamepy/models/calculate.py
@@ -39,11 +39,25 @@ class Calcular:
 
     @property
     def _gerar_valor(self: object) -> int:
-        pass
+        if self.dificuldade == 1:
+            return randint(0, 10)
+        elif self.dificuldade == 2:
+            return randint(0, 100)
+        elif self.dificuldade == 3:
+            return randint(0, 1000)
+        elif self.dificuldade == 4:
+            return randint(0, 10000)
+        else:
+            return randint(0, 100000)
 
     @property
     def _gerar_resultado(self: object) -> int:
-        pass
+        if self.operacao == 1:
+            return self.valor1 + self.valor2
+        elif self.operacao == 2:
+            return self.valor1 - self.valor2
+        else:
+            return self.valor1 * self.valor2
 
     def __str__(self: object) -> str:
         op: str = ''
@@ -62,8 +76,26 @@ class Calcular:
             f'Operação: {op!r}'
         )
 
+    @property
+    def _op_simbolo(self: object) -> str:
+        if self.operacao == 1:
+            return '+'
+        elif self.operacao == 2:
+            return '-'
+        else:
+            return '*'
+
     def checar_resultado(self: object, resposta: int, /) -> bool:
-        pass
+        certo: bool = False
+        if self.resultado == resposta:
+            print('Resposta correta!')
+            certo = True
+        else:
+            print('Resposta errada!')
+        print(
+            f'{self.valor1} {self._op_simbolo} {self.valor2} = {self.resultado}'
+        )
+        return certo
 
     def mostrar_operacao(self: object) -> None:
-        pass
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = ?')
